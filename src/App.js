aad4874fd30178b35767b41b00957f74fe18af89
jesscz <notionj.jc@gmail.com>
2023-06-07 17:58:11 -0400

    Add one pokemon image from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const randomNum = Math.round(Math.random() * 100);
+  const pokemonImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNum}.png`;
+  
+  // const fetchPokemonData = () => {
+  //   fetch(``)
+  //   .then(response => {
+  //     return response
+  //   })
+  // }
+
   return (
-    <div className="App">
-      
+    <div id="App">
+      <h1>Click a Pokémon</h1>
+      <img src={pokemonImgUrl}></img>
     </div>
   );
 }
